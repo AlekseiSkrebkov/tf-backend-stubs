3d7552c48bac5121a62f1f88cf5570317e7e055d
Alexey Skrebkov <alexeyskrebkov@Alexeys-MacBook-Pro.local>
2016-07-04 17:26:22 +0400

    static attributes list added

---
+++ data/static/attributes.js
@@ -0,0 +1,22 @@
+module.exports = [
+	{
+		name: "Hazardous",
+		type: "boolean"
+	},
+	{
+		name: "Fragile",
+		type: "boolean"
+	},
+	{
+		name: "High Value",
+		type: "boolean"
+	},
+	{
+		name: "Call Number",
+		type: "string"
+	},
+	{
+		name: "Number of Stops",
+		type: "number"
+	}
+]
