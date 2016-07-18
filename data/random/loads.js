a8ef218334859e95d6877a6bab8f302db4c09a04
Alexey Skrebkov <alexeyskrebkov@Alexeys-MacBook-Pro.local>
2016-07-18 13:05:45 +0400

    loads quantity is 200

--- data/random/loads.js
+++ data/random/loads.js
@@ -10,7 +10,7 @@ const divisions = require('../static/divisions.js')
 
 const numberOfStops = 7
 const numberOfShipments = 10
-const loadsQuantity = 1
+const loadsQuantity = 200
 
 var loads_collection = []
 for (var i = 0; i < loadsQuantity; i++) {
