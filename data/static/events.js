6e6847b1f4945ebd73467649f199f0974c92b2ca
Alexey Skrebkov <alexeyskrebkov@Alexeys-MacBook-Pro.local>
2016-07-22 12:11:11 +0400

    GET, POST /loads/:id/share and GET /loads/:id/notifications supported

---
+++ data/static/events.js
@@ -0,0 +1,32 @@
+module.exports = [
+	{
+		"id": 1,
+		"name": 'Departed',
+		"group": 'Load'
+	},
+	{
+		"id": 2,
+		"name": 'Delivered' ,
+		"group": 'Load'
+	},
+	{
+		"id": 3,
+		"name": 'Picked Up',
+		"group": 'Package'
+	},
+	{
+		"id": 4,
+		"name": 'Droped Off',
+		"group": 'Package'
+	},
+	{
+		"id": 5,
+		"name": 'Arrived',
+		"group": 'Stop'
+	},
+	{
+		"id": 6,
+		"name": 'Left',
+		"group": 'Stop'
+	}
+]
